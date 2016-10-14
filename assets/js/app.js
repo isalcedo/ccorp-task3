a811b95aea688f4b6253ed2fc921f03bf36ef6cd
isalcedo <ignacio@isalcedo.com>
2016-10-14 10:26:30 -0400

    Final

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,29 @@
 /*
-jQuery for animations and DOM simple stuff.
-AngularJS for complex stuff. 
- */
+*
+* PSD to HTML5 + CSS3 + jQuery
+* By Ignacio Salcedo - ignacio@isalcedo.com / http://isalcedo.com - https://orucode.com
+* Oct 2016.
+* for CompuCorp - Task 3.
+*
+*/
 $(document).foundation()
 
+$(document).ready( function(){
+
+	$('.cont-members-slider').slick({
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		arrows: false,
+		dots: true,
+	});
+
+	$('.cont-blog-slider').slick({
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 2000,
+	});
+
+});
